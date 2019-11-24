0af6dc04f29864de351edae1e1f6e3616e5e975d
studentwsb <studentwsb@STUDENTWSB>
2019-11-24 05:26:33 +0100

    Add project files.

---
+++ Appka_z_pierwszych_zajec/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Appka_z_pierwszych_zajec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
